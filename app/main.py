e371a6f86236700d72f8828c5ebf168b64b69d87
reold <redicrafty@gmail.com>
2024-06-21 16:00:10 +0530

    + CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .router import account, feed, inventory
 
 app = FastAPI()
 
+# CORS setup
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"], allow_credentials=True)
+
+# router setup
 app.include_router(account.router, prefix="/api")
 app.include_router(feed.router, prefix="/api")
 app.include_router(inventory.router, prefix="/api")
